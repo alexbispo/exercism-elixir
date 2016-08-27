1b210e8f573fe62d9f1e9a2d2df78d6f0554a60b
alexbispo <alexbispo.artes@gmail.com>
2016-08-27 19:12:48 -0300

    All tests for NucleotideCount.count/2 are working

---
+++ nucleotide-count/nucleotide_count.exs
@@ -0,0 +1,35 @@
+defmodule NucleotideCount do
+  @nucleotides [?A, ?C, ?G, ?T]
+
+  @doc """
+  Counts individual nucleotides in a NucleotideCount strand.
+
+  ## Examples
+
+  iex> NucleotideCount.count('AATAA', ?A)
+  4
+
+  iex> NucleotideCount.count('AATAA', ?T)
+  1
+  """
+  @spec count([char], char) :: non_neg_integer
+  def count(strand, nucleotide) do
+    unless(Enum.member?(@nucleotides, nucleotide), do: raise(ArgumentError, message: "invalid nucleotide"))
+    unless(Enum.all?(strand, &(Enum.member?(@nucleotides, &1))), do: raise(ArgumentError, message: "invalid strand"))
+    Enum.count(strand, &(&1 == nucleotide))
+  end
+
+
+  @doc """
+  Returns a summary of counts by nucleotide.
+
+  ## Examples
+
+  iex> NucleotideCount.histogram('AATAA')
+  %{?A => 4, ?T => 1, ?C => 0, ?G => 0}
+  """
+  @spec histogram([char]) :: map
+  def histogram(strand) do
+
+  end
+end
